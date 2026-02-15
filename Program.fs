397486791b6de63a50ec8a53880f61c369ba8cfb
Walby12 <alby09.bonf@gmail.com>
2026-02-15 22:57:37 +0100

    Started working on the parser

--- Program.fs
+++ Program.fs
@@ -4,12 +4,13 @@ open System.IO
 type TokenType =
     | Ident of string
     | Interger of int
+    | EOF
 
 type TopLevel = {
     Src: string
     mutable Index: int
     mutable Line: int
-    mutable Tokens: ResizeArray<TokenType>
+    mutable Token: TokenType
 }
 
 let readFile (path: string) =
@@ -42,20 +43,34 @@ let rec tokenize (tl: TopLevel) =
                 buff <- buff + (string tl.Src[tl.Index])
                 tl.Index <- tl.Index + 1
                 
-            tl.Tokens.Add(Interger (int buff))
-            tokenize tl
+            tl.Token <- Interger (int buff)
         | c when Char.IsLetter(c) ->
             let mutable buff = ""
             while tl.Index < tl.Src.Length && Char.IsLetterOrDigit(tl.Src[tl.Index]) && not (Char.IsWhiteSpace(tl.Src[tl.Index])) do
                 buff <- buff + (string tl.Src[tl.Index])
                 tl.Index <- tl.Index + 1
                 
-            tl.Tokens.Add(Ident (buff))
-            tokenize tl
+            tl.Token <- Ident (buff)
         | _ ->
             printfn "ERROR at line %d: Unknow char '%c'" tl.Line c
             exit 1
-   
+    else
+        tl.Token <- EOF
+        
+let rec parse (tl: TopLevel) =
+    tokenize tl
+    match tl.Token with
+    | EOF -> 
+        printfn "FINISHED"
+    | Ident op ->
+        match op with
+        | "mov" -> printfn "NICE mov"
+        | _ ->
+            printfn "ERROR at line %d: Unknow op '%s'" tl.Line op
+    | Interger i ->
+        printfn "Found standalone integer: %d" i
+        parse tl
+
 [<EntryPoint>]
 let main _ =
     match readFile "test.on" with
@@ -64,10 +79,9 @@ let main _ =
             Src = res
             Index = 0
             Line = 1
-            Tokens = ResizeArray<TokenType>()
+            Token = Ident ("START")
         }
-        tokenize tl
-        printfn "Tokens: %+A" tl.Tokens
+        parse tl
     | None -> exit 1
 
     0
